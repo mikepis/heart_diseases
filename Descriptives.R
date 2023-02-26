6243c435881836b5aacb27ee217897391c56da5d
mikepis <126319639+mikepis@users.noreply.github.com>
2023-02-27 01:55:55 +0300

    Add files via upload

---
+++ Descriptives.R
@@ -0,0 +1,10 @@
+names(Township)
+summary(Township$`Annual Household Income`)
+table(Township$`History of Heart Disease`)
+barplot(table(Township$`History of Heart Disease`))
+table(Township$`Home Size`)
+barplot(table(Township$`Home Size`))
+table (Township $Race)
+barplot(table(Township$Race))
+
+
